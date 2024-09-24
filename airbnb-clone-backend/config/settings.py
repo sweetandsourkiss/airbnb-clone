fc59d2ee01a44311a8526d21eae0453d0bf1ad3c
sweetandsourkiss <sweetandsourkiss@gmail.com>
2024-09-24 16:17:29 +0900

    #5 USERS APP

--- airbnb-clone-backend/config/settings.py
+++ airbnb-clone-backend/config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 CUSTOM_APPS = [
-    "houses.apps.HousesConfig",
+    "users.apps.UsersConfig",
 ]
 
 SYSTEM_APPS = [
@@ -128,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Auth
+AUTH_USER_MODEL = "users.User"
