7b866f952a3c35f265329e8e37e7bbdeba1e7026
Jay Son <sweetandsourkiss@gmail.com>
2024-09-27 00:27:24 +0900

    #6 MODELS AND ADMIN

--- airbnb-clone-backend/config/settings.py
+++ airbnb-clone-backend/config/settings.py
@@ -36,6 +36,11 @@ CUSTOM_APPS = [
     "rooms.apps.RoomsConfig",
     "experiences.apps.ExperiencesConfig",
     "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
+    "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
 SYSTEM_APPS = [
