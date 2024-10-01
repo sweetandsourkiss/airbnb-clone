f3a4be9be65d64968d083f74716d8f557110dce7
Jay Son <sweetandsourkiss@gmail.com>
2024-10-01 22:06:43 +0900

    #8 POWER ADMIN

--- airbnb-clone-backend/rooms/admin.py
+++ airbnb-clone-backend/rooms/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
         "kind",
         "total_amenities",
+        "rating",
         "owner",
         "created_at",
     )
@@ -19,6 +29,12 @@ class RoomAdmin(admin.ModelAdmin):
         "pet_friendly",
         "kind",
         "amenities",
+    )
+
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
     )
 
 
